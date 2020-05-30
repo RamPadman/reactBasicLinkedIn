ba676271694f0f193b0a9d5695c6260ac2bd6279
RamPadman <RamPadman@users.noreply.github.com>
2020-05-30 15:32:32 +0200

    build file react

---
+++ build/precache-manifest.9ddbd712f77cb3c864f860bf5eb7a445.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "802eb8c4d8b0609e31f1ea5afdd595b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08e06816e25377427f0a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6f751166d507ac03507f",
+    "url": "/static/js/2.b865a7a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b865a7a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08e06816e25377427f0a",
+    "url": "/static/js/main.5c5ebf3f.chunk.js"
+  },
+  {
+    "revision": "67c46fa5191a7506d6bc",
+    "url": "/static/js/runtime-main.bbba985a.js"
+  }
+]);
